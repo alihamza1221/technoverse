5719dc99aea1ef9f0bc9e1e574c784844d26cc2f
alihamza1221 <alihamza77065@gmail.com>
2025-05-31 17:24:51 +0500

    fix

--- backend/routes/issues.routes.js
+++ backend/routes/issues.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const Issue = require("../models/issue.model");
 router.get("/", async (req, res) => {
   try {
     const issues = await Issue.find({})
